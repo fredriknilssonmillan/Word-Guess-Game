9374659339c535f7c53ed3b88dace1fa28063ce8
FRENIMI <fredrik.nilsson@docsolutions.com>
2019-03-16 01:25:46 -0600

    putting some design to it, still not finished

--- javascript/game2.js
+++ javascript/game2.js
@@ -3,6 +3,10 @@
 var computerChoices = ["Eric Prydz", "Adam Beyer", "Tiesto", "Armin van Buuren", "Carl Cox", "Kungs", "Above Beyond", "Nora en Pure", "Camelphat", "Steve Aoki", "Jamie Jones",
     "Charlotte de Witte", "Paul Oakenfold", "Daft Punk", "Sebastian Ingrosso", "Paul van Dyk", "Steve Angello", "Alan Walker"];
 
+// Creates an array that lists out all the letters the user can push.
+var options = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "V", "X", "Y", "Z"];
+
+//variables
 var computerGuess;
 var lines = [];
 var gfar = [];
@@ -11,9 +15,10 @@ var characters;
 var wins = 0;
 var losses = 0;
 
+//starting the game with a random choice and resetting values, this function will be invoked every time after user wins or loses.
 aleatorio();
 
-//Making the computer choose a DJ
+//creating the function for starting the game with a random choice and resetting values
 function aleatorio() {
     gleft = 10;
     lines = [];
@@ -21,7 +26,7 @@ function aleatorio() {
     computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)].toUpperCase();
     characters = computerGuess.length;
 
-    // I had trouble with the spacing (" ")... i got help in this part by Manuel Castillo.
+    // This is to substitute the computer choice with lines. I had trouble with the spacing (" ")... i got help in this part by Manuel Castillo.
     for (i = 0; i < computerGuess.length; i++) {
         if (computerGuess[i] === " ") {
             lines.push(" ");
@@ -30,60 +35,59 @@ function aleatorio() {
             lines.push("_");
         }
     }
-    document.getElementById("Current-word").innerHTML = lines.join("");
-
-    //No te olvides de comentar el console.log para que el usuario no pueda hacer trampa.
-   // console.log(computerGuess);
+    dibujar();
 }
 
-
-
-// esta es nueva, tambièn la aprendì de Manuel: /[^a-zA-Z]/ 
+//whenever the user pushes a key we have to check that it is a valid key and if it is a key in the word, otherwise add it to guesses so far without repeating
 document.onkeydown = function (event) {
-    if (gleft > 0 && !/[^a-zA-Z]/.test(event.key)) {
+    var letterChosen = event.key.toUpperCase();
+    for (var i = 0; i < computerGuess.length; i++) {
 
-        //Lo que sigue lo estuve viendo con Rafa
-        guessing(event.key.toUpperCase());
-        if ((lines.join("") == computerGuess) && (gleft > 0)) {
-            wins++;
-            alert("Great! You Won! " + computerGuess + " was the artist, you know your DJ's");
-            if (wins === 1) {
-                document.getElementById("Wins").innerHTML = wins + " DJ guessed";
-            }
-            else {
-                document.getElementById("Wins").innerHTML = wins + " DJ's guessed";
-            }
-
-            aleatorio();
-        }
-        else if (gleft == 0) {
-            losses++;
-            alert("You lost! It seems you like Reguetton. What a shame, you do not deserve to listen to " + computerGuess);
-            document.getElementById("Losses").innerHTML = "Reguetton score (this is bad and shameful): " + losses;
-            aleatorio();
+        if (letterChosen === computerGuess[i]) {
+            lines[i] = letterChosen
         }
     }
+
+//This is so that if the user repeats a key nothing happens in the count or the list of guesses left
+    if (options.indexOf(letterChosen) != -1) {
+        if (computerGuess.indexOf(letterChosen) === -1) {
+            if (gfar.indexOf(letterChosen) === -1) {
+                gfar.push(letterChosen);
+                gleft--;
+            }
+        }
+    }
+
+//Decides that the user wins when the "lines" = "computer guess"
+    if (lines.join("") == computerGuess) {
+        wins++;
+        dibujar();
+        alert("Great! You Won! " + computerGuess + " was the artist, you know your DJ's");
+        aleatorio();
+    }
+
+//The user loses when counter of gleft reaches 0
+    if (gleft == 0) {
+        losses++;
+        dibujar();
+        alert("You lost! It seems you like Reguetton. What a shame, you do not deserve to listen to " + computerGuess);
+        aleatorio();
+    }
+    dibujar();
 }
 
-//creo que esta la puedo simplificar màs
-function guessing(yesornoguess) {
-    var stroke = 0;
-    if (gfar.includes(yesornoguess)) {
-        stroke++;
+//this function is in order to write the text each time it is requiered in the html
+function dibujar() {
+    document.getElementById("Current-word").innerHTML = lines.join("");
+    document.getElementById("Userchoice-text").innerHTML = "Your used letters so far are: <br>" + gfar.join(" ");
+    document.getElementById("Guesses-left").innerHTML = "Remaining attempts: " + gleft;
+    document.getElementById("Losses").innerHTML = "Reguetton score (this is bad and shameful): " + losses;
+
+//this is just to us an apostrophe when it's plural and not use it when it is singular
+    if (wins === 1) {
+        document.getElementById("Wins").innerHTML = wins + " DJ guessed";
     }
     else {
-        for (i = 0; i < computerGuess.length; i++) {
-            if (computerGuess[i] == yesornoguess) {
-                lines[i] = yesornoguess;
-                stroke++;
-            }
-        }
-        gfar.push(yesornoguess);
-        document.getElementById("Current-word").innerHTML = lines.join("");
+        document.getElementById("Wins").innerHTML = wins + " DJ's guessed";
     }
-    if (stroke == 0) {
-        gleft--;
-    }
-    document.getElementById("Userchoice-text").innerHTML = gfar.join(" ");
-    document.getElementById("Guesses-left").innerHTML = "Remaining attempts: " + gleft;
 }
